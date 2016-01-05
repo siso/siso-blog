d034f19e52e648898a6dc09eebce6256cb25da64
myself <siso@rdl0.localhost>
2016-01-05 11:51:44 +1100

    Apply Bootstrap2 theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,3 +33,6 @@ DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+import os.path
+THEME = os.path.expanduser('~/workspace/pelican-themes/bootstrap2')
